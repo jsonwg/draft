37a70ec6afa27c6bd3224cb4bc53b4a31af10a5a
Jason <ja2415n@gmail.com>
2021-10-18 22:59:39 -0700

    Add container transparency

--- src/components/Container.svelte
+++ src/components/Container.svelte
@@ -6,9 +6,19 @@
     justify-content: center;
     align-items: center;
     background-color: var(--bg);
-    box-shadow: 0 0 13px black;
-    border-radius: 30px;
-    outline: 0.25vh solid white;
-    outline-offset: -3vh;
+    box-shadow: 1.5em 1em 10px rgba(0, 0, 0, 0.6);
+    border-radius: 20px;
+    border: solid 0.5vh transparent;
+    padding: 10px;
+    background-clip: padding-box;
+    outline: 1vh solid var(--bg);
+    outline-offset: 10;
+    margin: 1vh;
+  }
+
+  @media (max-width: 768px) {
+    div {
+      box-shadow: 0.9em 0.8em 10px rgba(0, 0, 0, 0.7);
+    }
   }
 </style>
